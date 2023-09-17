1579dfb004d0e1febb57c235b1d1ed7053d78c12
shunsuke <snskngo03.sch.2021@gmail.com>
2023-09-17 20:52:38 +0900

    キャラクター見た目置き換え

---
+++ Assets/Scripts/Character/View/CharacterView.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UniRx;
+
+namespace Character.View
+{
+    public class CharacterView : MonoBehaviour
+    {
+        [SerializeField] private JumpCharacterHeightModel _character;
+        [SerializeField] private BorderLine _borderLine;
+        [SerializeField] private GameObject _borderLineView;
+        [SerializeField] private float _maxLocalHeight;
+
+        private async void Awake()
+        {
+            await UniTask.WaitUntil(() => _character.IsInitialized);
+
+            _character.HeightObservable.Subscribe(height =>
+            {
+                var characterLocalHeight = (height - _borderLine.HeightObservable.Value);
+                characterLocalHeight += _borderLineView.transform.localPosition.y;
+
+                if (characterLocalHeight < _maxLocalHeight) 
+                {
+                    transform.localPosition = new Vector3(transform.localPosition.x, characterLocalHeight, transform.localPosition.z);
+                }
+                else
+                {
+                    transform.localPosition = new Vector3(transform.localPosition.x, _maxLocalHeight, transform.localPosition.z);
+                }
+            }).AddTo(this);
+        }
+    }
+}
